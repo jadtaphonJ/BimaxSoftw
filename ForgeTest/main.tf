638690778f3f658248108ccbf932b5c79e73dd1c
jadtaphonJ <jadtaphonchinnapan@gmail.com>
2020-09-22 14:36:07 +0700

    2020_09_22

---
+++ ForgeTest/main.tf
@@ -0,0 +1,25 @@
+
+provider "heroku" {
+  version = "~> 2.0"
+}
+
+variable "app_name" {
+  default = "myweb-101"
+}
+
+resource "heroku_app" "myweb" {
+  name   = "${var.app_name}"
+  region = "us"
+  stack  = "container"
+}
+
+resource "heroku_build" "myweb" {
+  app = "${heroku_app.myweb.name}"
+  source = {
+    path = "./"
+  }
+}
+
+output "app_url" {
+  value = "https://${heroku_app.myweb.name}.herokuapp.com"
+}
